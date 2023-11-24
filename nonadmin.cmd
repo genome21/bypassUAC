96af18901b9b9d02debfa291664bb5031e78cb6f
Mikey <151540025+MikeyxDazai@users.noreply.github.com>
2023-11-24 18:11:01 +0530

    1 nonadmin.cmd

--- nonadmin.cmd
+++ nonadmin.cmd
@@ -1,1 +1,1 @@
-cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "[insert_program_filename]""
+cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "[BlueStacksInstaller_5.14.0.1061_native_de92e9873b0b254d455dcf1d6262978b_MzsxNSwwOzUsMTsxNSw0OzE1]""
